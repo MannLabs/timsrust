1d619cebdfdeee7b21d703ca09df8d6bba11d973
J. Sebastian Paez <jspaezp@gmail.com>
2023-10-17 16:39:51 -0700

    added some docs

--- src/frames.rs
+++ src/frames.rs
@@ -10,6 +10,25 @@ pub struct Frame {
     pub frame_type: FrameType,
 }
 
+
+/// A Frame window is a group of scans (subset of frame along IMS)
+/// that are associated with a single isolation window.
+/// 
+/// * `scan_offsets` - The scan offsets for the frame window.
+/// * `tof_indices` - The TOF indices for the frame window.
+///     These values are correlated with the m/z values of the peaks
+///     but are kept as u32 to save space (converting them makes them f64).
+/// * `intensities` - The intensities for the frame window.
+/// * `frame_index` - The index of the frame.
+/// * `rt` - The retention time of the frame.
+/// * `window_group` - The window group of the frame window.
+///     This is an integer value that originally comes from the "WindowGroup"
+///     in the `dia_frame_msms_table` of the TDF file.
+/// * `scan_start` - The index of the first scan in the frame window.
+///     This is used to know what offset to use when converting ims
+///     values from the scan offsets (this number should converted
+///     to mobility and all other mobility values within this frame window
+///     should have that added).
 pub struct FrameMSMSWindow {
     pub scan_offsets: Vec<u64>,
     pub tof_indices: Vec<u32>,
@@ -35,6 +54,12 @@ impl Default for FrameType {
 }
 
 impl Frame {
+    /// Resolves the m/z values for the frame.
+    /// 
+    /// * `mz_reader` - The TOF to m/z converter.
+    /// 
+    /// # Returns
+    /// A vec with the m/z values for the frame (as f64).
     pub fn resolve_mz_values(&self, mz_reader: &Tof2MzConverter) -> Vec<f64> {
         self.tof_indices.iter().map(|&x| mz_reader.convert(x)).collect()
     }
