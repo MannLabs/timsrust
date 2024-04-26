997385bd06c73a7d2a7209de1721e634cc67f9ab
Sander Willems <sander.willems@bruker.com>
2024-04-26 15:51:34 +0200

    FEAT: new sql reader module

---
+++ src/io/readers/common/sql_reader.rs
@@ -0,0 +1,38 @@
+use std::path::{Path, PathBuf};
+
+use rusqlite::Connection;
+
+#[derive(Debug)]
+pub struct SqlReader {
+    path: PathBuf,
+    connection: Connection,
+}
+
+impl SqlReader {
+    pub fn open(file_name: impl AsRef<Path>) -> Result<SqlReader, SqlError> {
+        let path = file_name.as_ref().to_path_buf();
+        let connection = Connection::open(&path)?;
+        Ok(Self { path, connection })
+    }
+}
+
+pub trait SqlReadable {
+    fn get_sql_query() -> String;
+
+    fn from_sql_row(row: &rusqlite::Row) -> Self;
+
+    fn from_sql_reader(reader: &SqlReader) -> Result<Vec<Self>, SqlError>
+    where
+        Self: Sized,
+    {
+        let query = Self::get_sql_query();
+        let mut stmt = reader.connection.prepare(&query)?;
+        let rows = stmt.query_map([], |row| Ok(Self::from_sql_row(row)))?;
+        let result = rows.collect::<Result<Vec<_>, _>>()?;
+        Ok(result)
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+#[error("SqlError: {0}")]
+pub struct SqlError(#[from] rusqlite::Error);
