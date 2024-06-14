3032d6d329d5ef9b9dd14b742f302b2d18134009
Sander Willems <sander.willems@bruker.com>
2024-06-14 13:22:14 +0200

    DOCS: typo

--- src/domain_converters.rs
+++ src/domain_converters.rs
@@ -7,7 +7,7 @@ pub use frame_to_rt::Frame2RtConverter;
 pub use scan_to_im::Scan2ImConverter;
 pub use tof_to_mz::Tof2MzConverter;
 
-/// Convert from one domain (e.g. Time of Flight) to a another (m/z).
+/// Convert from one domain (e.g. Time of Flight) to another (m/z).
 pub trait ConvertableDomain {
     fn convert<T: Into<f64> + Copy>(&self, value: T) -> f64;
 }
