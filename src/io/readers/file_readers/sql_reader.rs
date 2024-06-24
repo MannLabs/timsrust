bfe71ccfc781c041e6b1a7fbd12813bf8bbf5f6c
Sander Willems <sander.willems@bruker.com>
2024-06-24 14:00:21 +0200

    FEAT: added hashmap reader for sql

--- src/io/readers/file_readers/sql_reader.rs
+++ src/io/readers/file_readers/sql_reader.rs
@@ -3,7 +3,10 @@ pub mod metadata;
 pub mod pasef_frame_msms;
 pub mod precursors;
 
-use std::path::{Path, PathBuf};
+use std::{
+    collections::HashMap,
+    path::{Path, PathBuf},
+};
 
 use rusqlite::Connection;
 
@@ -43,6 +46,26 @@ pub trait ReadableSqlTable {
         let rows = stmt.query_map([], |row| Ok(Self::from_sql_row(row)))?;
         let result = rows.collect::<Result<Vec<_>, _>>()?;
         Ok(result)
+    }
+}
+
+pub trait ReadableSqlHashMap {
+    fn get_sql_query() -> String;
+
+    fn from_sql_reader(
+        reader: &SqlReader,
+    ) -> Result<HashMap<String, String>, SqlError>
+    where
+        Self: Sized,
+    {
+        let query = Self::get_sql_query();
+        let mut stmt = reader.connection.prepare(&query)?;
+        let kv_map: HashMap<String, String> = stmt
+            .query_map([], |row| Ok((row.get(0)?, row.get(1)?)))?
+            .map(Result::unwrap)
+            .collect();
+
+        Ok(kv_map)
     }
 }
 
