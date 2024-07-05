2a6ff12a95cb031c132605f01fc4873beee54a49
Sander Willems <sander.willems@bruker.com>
2024-07-05 13:15:57 +0200

    FEAT: option to filter spectra by top n peaks

--- src/ms_data/spectra.rs
+++ src/ms_data/spectra.rs
@@ -11,3 +11,34 @@ pub struct Spectrum {
     pub isolation_mz: f64,
     pub isolation_width: f64,
 }
+
+impl Spectrum {
+    pub fn get_top_n(&self, top_n: usize) -> Self {
+        let mut indexed: Vec<(f64, usize)> =
+            self.intensities.iter().cloned().zip(0..).collect();
+        indexed.sort_by(|a, b| {
+            b.0.partial_cmp(&a.0).unwrap_or(std::cmp::Ordering::Equal)
+        });
+        let mut top_indices: Vec<usize> = indexed
+            .iter()
+            .take(top_n)
+            .map(|&(_, index)| index)
+            .collect();
+        top_indices.sort();
+        Spectrum {
+            mz_values: top_indices
+                .iter()
+                .map(|&index| self.mz_values[index])
+                .collect(),
+            intensities: top_indices
+                .iter()
+                .map(|&index| self.intensities[index])
+                .collect(),
+            precursor: self.precursor,
+            index: self.index,
+            collision_energy: self.collision_energy,
+            isolation_mz: self.isolation_mz,
+            isolation_width: self.isolation_width,
+        }
+    }
+}
