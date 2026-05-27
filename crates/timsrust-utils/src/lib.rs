a9fd0190258d97d8a5a29987fd43ad91565394c8
Sander Willems <sander.willems@bruker.com>
2026-05-27 15:01:10 +0200

    feta: timsrust_utils

---
+++ crates/timsrust-utils/src/lib.rs
@@ -0,0 +1,16 @@
+pub mod error;
+pub mod graph_buffer;
+pub mod hash_sets;
+pub mod ndarray;
+pub mod reader;
+pub mod thread;
+pub mod vec;
+
+// #[macro_export]
+// macro_rules! getter {
+//     ($name:ident, $field_ty:ty) => {
+//         fn $name(&self) -> &$field_ty {
+//             &self.$name
+//         }
+//     };
+// }
